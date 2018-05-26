e920adf6f060b2d6b08134bc15ac31b40c54fe2c
uhyo <uhyo@uhy.ooo>
2018-05-26 23:41:41 +0900

    export more

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,6 @@
 export { register } from './components/register.js';
 export { OneKey } from './components/one-key.js';
+export { KeyConfigTable } from './components/key-table.js';
+export { KeyConfig } from './components/key-config.js';
 export { ChromeStorageStore } from './stores/chrome-storage.js';
+export { LocalStorageStore } from './stores/local-storage.js';
