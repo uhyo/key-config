005b6eaeb32136e717c83040a9f031762807cc2c
uhyo <uhyo@uhy.ooo>
2018-05-27 20:28:25 +0900

    add .npmignore

--- lib/index.ts
+++ lib/index.ts
@@ -4,3 +4,4 @@ export { KeyConfigTable } from './components/key-table.js';
 export { KeyConfig } from './components/key-config.js';
 export { ChromeStorageStore } from './stores/chrome-storage.js';
 export { LocalStorageStore } from './stores/local-storage.js';
+export { IOneKeySpec, KeyConfigSpec } from './spec.js';
